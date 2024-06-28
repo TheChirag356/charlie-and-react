085327a56884ceb419bc6c8a035ed06e3413bd6e
TheChirag356 <arorachirag12005@gmail.com>
2024-06-28 15:16:36 +0530

    Chnages from video 2

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chai aur react | Chirag Arora</h1>
+      <h2>What i learnt in this video was that react is not some crazy abba-jabba-dabba. It is easy to understand if we put in the right effort.</h2>
+    </div>
+  );
+}
+
+export default App;
